a91bf64b82099b2877511c80f400296b422fbcfc
ErElectical <mohittanwar786.mt@gmail.com>
2021-12-30 07:47:55 +0000

    insertion into linkedlist

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DataStructureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.InsertLast(70);
+            customLinkedList.InsertLast(30);
+            customLinkedList.InsertLast(56);
+            customLinkedList.Display();
+            Console.ReadLine();
+        }
+    }
+}
