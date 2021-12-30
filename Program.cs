5c8ab5d37b77316f3053da0dd9fddfd507b2c244
ErElectical <mohittanwar786.mt@gmail.com>
2021-12-30 08:31:35 +0000

    delete last node from linked list

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DataStructureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.InsertLast(56);
+            customLinkedList.InsertLast(30);
+            customLinkedList.InsertLast(70);
+            Console.WriteLine("Linked list before deletion:");
+            customLinkedList.Display();
+            customLinkedList.DeleteLastNode();
+            Console.WriteLine("Linked list after deletion:");
+            customLinkedList.Display();
+            Console.ReadLine();
+        }
+    }
+}
