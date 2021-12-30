3f1426f83413631d30e70bb87ce174219cf10015
ErElectical <mohittanwar786.mt@gmail.com>
2021-12-30 08:01:20 +0000

    append in linked list

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DataStructureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.Append(56);
+            customLinkedList.Append(30);
+            customLinkedList.Append(70);
+            customLinkedList.Display();
+            Console.ReadLine();
+        }
+    }
+}
