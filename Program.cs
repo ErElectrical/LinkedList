9700d6957048331ee386b7cdec1d6840a959b9b3
ErElectical <mohittanwar786.mt@gmail.com>
2021-12-30 08:49:18 +0000

    search node in linked list

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DataStructureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.InsertLast(56);
+            customLinkedList.InsertLast(30);
+            customLinkedList.InsertLast(70);
+            Console.WriteLine(customLinkedList.Search(30));
+            Console.ReadLine();
+        }
+    }
+}
